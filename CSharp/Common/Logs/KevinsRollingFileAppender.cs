761d0d357aa8766a92644b69bdbc075a892698c3
Kevin <kevin.t.echols@gmail.com>
2016-09-22 12:35:33 -0400

    Added missing RollingFilleAppender to the examples solution

---
+++ CSharp/Common/Logs/KevinsRollingFileAppender.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Kevins.Examples.Common.Extensions;
+
+namespace Kevins.Examples.Common.Logs
+{
+    public class KevinsRollingFileAppender : log4net.Appender.RollingFileAppender
+    {
+        protected override void AdjustFileBeforeAppend()
+        {
+            // delete old files
+            var logDirectory = Path.GetDirectoryName(File);
+            foreach (var file in Directory.GetFiles(logDirectory, "*.log"))
+            {
+                if (DateTime.Now.Subtract(System.IO.File.GetLastWriteTime(file)).Days >= "NumberDaysToKeepLogs".Setting().AsInt())
+                {
+                    DeleteFile(file);
+                }
+            }
+
+            base.AdjustFileBeforeAppend();
+        }
+    }
+}
