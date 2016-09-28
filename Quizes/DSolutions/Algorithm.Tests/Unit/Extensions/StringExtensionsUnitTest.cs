2ec3d24b71adf45e8987ca419d50ec4e21d2d929
Kevin <kevin.t.echols@gmail.com>
2016-09-28 15:39:41 -0400

    Adding Algorithm solution

---
+++ Quizes/DSolutions/Algorithm.Tests/Unit/Extensions/StringExtensionsUnitTest.cs
@@ -0,0 +1,36 @@
+using Kevins.DSolutions.Algorithm.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kevins.DSolutions.Algorithm.Tests.Unit.Extensions
+{
+    [TestClass]
+    public class StringExtensionsUnitTest
+    {
+        [TestMethod]
+        public void ShouldFindFirstStringPairAtTheEnds()
+        {
+            Assert.AreEqual("Kevin", "KevinEcholsEcholsKevin".FindFirstStringPair(5));
+        }
+
+
+        [TestMethod]
+        public void ShouldFindFirstStringPairInTheMiddle()
+        {
+            Assert.AreEqual("Echols", "KevinEcholsEcholsKevin".FindFirstStringPair(6));
+        }
+
+
+        [TestMethod]
+        public void ShouldNotIfMatchLengthIsZero()
+        {
+            Assert.AreEqual(string.Empty, "Kevin".FindFirstStringPair(0));
+        }
+
+
+        [TestMethod]
+        public void ShouldNotFindSearchStringIsEmpty()
+        {
+            Assert.AreEqual(string.Empty, string.Empty.FindFirstStringPair(10));
+        }
+    }
+}
