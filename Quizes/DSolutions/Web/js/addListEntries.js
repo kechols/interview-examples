b37293eac1e78b86e849f0060fdb12c7082eb0b8
Kevin <kevin.t.echols@gmail.com>
2016-09-29 10:29:35 -0400

    Added Answer I gavce to Web, Javascript, Css Quiz Question under web

---
+++ Quizes/DSolutions/Web/js/addListEntries.js
@@ -0,0 +1,34 @@
+/**
+ * Created by kechols on 9/29/2016.
+ *
+ *
+ * This "addListItem" file is a javascript that is responsibe for
+ * initializing the loading of the addListEntries.html content page. 
+ * 
+ * In particular it is resposible for making sure that the:
+ * -- add item button click behaviour occurs as described in the quiz question
+ * ----- no empty entries are added to the list box
+ * ----- every third entry is red
+ * ----- all other entries are black
+ *
+ *
+ */
+
+
+$(function () {
+    $("#addEntry").click(function () {
+        addNewEntry();
+    });
+});
+
+
+function addNewEntry() {
+    var newEntryValue = $("#newEntry").val().trim();
+    if (newEntryValue !== "") {
+        var styleClasses = "entryItem " + ($(".entryTem").length % 3 === 0 ? "redEntryItem" : "blackEntryItem");
+        $("#entryItems").append("<option class='" + styleClasses + "' value='" + newEntryValue + "'>" + newEntryValue + "</option>");
+    }
+}
+
+
+
