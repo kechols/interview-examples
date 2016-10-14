304c111511b2ec4d6a8be5563893167b531dda12
Kevin <kevin.t.echols@gmail.com>
2016-10-14 08:37:34 -0400

    Attemptig to make this github easier to navigate

---
+++ JQuery/As_Function_Bucket/js/addListEntries.js
@@ -0,0 +1,44 @@
+/**
+ * Created by kechols on 9/29/2016.
+ *
+ *
+ * This "addListItem" file is a javascript that is responsibe for
+ * initializing the loading of the addListEntries.html content page. 
+ * 
+ * In particular it is resposible for making sure that the:
+ * -- add item button click behaviour occurs as described in the quiz question
+ * ----- no empty entries are added to the list box
+ * ----- every third entry is red
+ * ----- all other entries are black
+ *
+ *
+ */
+
+
+$(function () {
+    $("#addEntry").click(function () {
+        addNewEntry();
+    });
+	
+	
+	$("#newEntry").keyup(function(keyUpEvent) {
+		if (keyUpEvent.keyCode === 13) {
+			addNewEntry();
+		}
+	});
+});
+
+
+function addNewEntry() {
+    var newEntryValue = $("#newEntry").val().trim();
+    if (newEntryValue !== "") {
+        var isEveryThird = (($(".entryItem").length + 1) % 3) === 0;
+        var styleClasses = "entryItem " + (isEveryThird ? "redEntryItem" : "blackEntryItem");
+        $("#entryItems").append("<option class='" + styleClasses + "' value='" + newEntryValue + "'>" + newEntryValue + "</option>");
+    } else {
+        alert("Entry must have none blank characters.");
+    }
+}
+
+
+
