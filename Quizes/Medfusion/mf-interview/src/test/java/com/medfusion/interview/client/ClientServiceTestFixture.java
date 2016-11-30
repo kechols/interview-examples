2efee8c93083323d4252e472dd00603a00a3897d
kechols <kevin.t.echols@gmail.com>
2016-11-30 13:04:42 -0500

    Forgot to checkin 2 files

---
+++ Quizes/Medfusion/mf-interview/src/test/java/com/medfusion/interview/client/ClientServiceTestFixture.java
@@ -0,0 +1,61 @@
+package com.medfusion.interview.client;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import com.medfusion.interview.data.DocumentReference;
+
+public class ClientServiceTestFixture {
+	private DocumentReference testDocument = null;
+	
+	private ClientServiceTestFixture (){}
+	
+	public static Date dateTimeNow () {
+		return new Date();
+	}
+	
+	public static ClientServiceTestFixture Instance(){
+		return new ClientServiceTestFixture();
+	}
+	
+	
+	public ClientServiceTestFixture createDocument(){
+		testDocument = new DocumentReference();
+		return this;
+	}
+	
+	
+	public DocumentReference createDocument(String id, String type, String created){
+		createDocument().withId(id).withType(type).created(created);
+		return testDocument;
+	}
+	
+	
+	public ClientServiceTestFixture withId(String id){
+		createIfNecessary();
+		testDocument.setId(id);
+		return this;
+	}
+	
+	
+	public ClientServiceTestFixture withType(String type){
+		createIfNecessary();
+		testDocument.setType(type);
+		return this;
+	}
+	
+	
+	public ClientServiceTestFixture created(String created){
+		createIfNecessary();
+		testDocument.setCreated(created);
+		return this;
+	}
+	
+	
+	private void createIfNecessary(){
+		if (testDocument == null){
+			createDocument();
+		}
+	}
+}
